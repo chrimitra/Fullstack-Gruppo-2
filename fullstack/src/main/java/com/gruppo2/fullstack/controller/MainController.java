0bee2ccaabe128772dbee1f806178ce0fe319196
Zir0Code <edoardoalessandro@gmail.com>
2022-11-29 15:49:45 +0100

    Mappatura pagine sul controller

--- fullstack/src/main/java/com/gruppo2/fullstack/controller/MainController.java
+++ fullstack/src/main/java/com/gruppo2/fullstack/controller/MainController.java
@@ -1,5 +1,57 @@
 package com.gruppo2.fullstack.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("")
 public class MainController {
 
+	//@Autowired
+	//private UserDao UserRepository;
+	
+	@GetMapping("")
+	public String index() {
+		return "index";
+	}
+	
+	@GetMapping("/index")
+	public String index2() {
+		return "index";
+	}
+
+	@GetMapping("/registrazione")
+	public String registrazione() {
+		return "registrazione";
+	}
+	
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+	
+	@GetMapping("/ciao")
+	public String ciao() {
+		return "ciao";
+	}
+	
+	
+	@GetMapping("/recuperapass")
+	public String recuperaPassword() {
+		return "recuperapass";
+	}
+	
+	@GetMapping("/confermatoken")
+	public String confarmaToken() {
+		return "confermatoken";
+	}
+	
+	
+	@GetMapping("/profilo")
+	public String profilo() {
+		return "profilo";
+	}
+	
 }
