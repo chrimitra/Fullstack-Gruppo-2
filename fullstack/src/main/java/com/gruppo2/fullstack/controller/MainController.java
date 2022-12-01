b82ac37de70c9eb5b1cc2bd2d8213030fbdf30ef
Christian <christianmitra7@gmail.com>
2022-12-01 12:27:47 +0100

    Edited HTML Login and Registration

--- fullstack/src/main/java/com/gruppo2/fullstack/controller/MainController.java
+++ fullstack/src/main/java/com/gruppo2/fullstack/controller/MainController.java
@@ -58,6 +58,7 @@ public class MainController {
 	// REGISTRAZIONE (solo admin)
 	@GetMapping("/registrazione")
 	public String registrazione() {
+		
 		return "registrazione";
 	}
 	
@@ -93,6 +94,7 @@ public class MainController {
 	public String report() {
 		return "report";
 	}
+	
 	
 }
 
