1ff703d0744245d5a28343cee50fba17c5d67ffc
xenos96 <amime16tablet@gmail.com>
2022-12-01 23:46:25 +0100

    sistemato i controller admin insegnante e studente

--- fullstack/src/main/java/com/gruppo2/fullstack/controller/AdminController.java
+++ fullstack/src/main/java/com/gruppo2/fullstack/controller/AdminController.java
@@ -1,10 +1,28 @@
 package com.gruppo2.fullstack.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.gruppo2.fullstack.Dao.DomandaDao;
+import com.gruppo2.fullstack.Dao.FeedbackDao;
+import com.gruppo2.fullstack.Dao.ModuloDao;
+import com.gruppo2.fullstack.Dao.RuoloDao;
+import com.gruppo2.fullstack.Dao.UtenteDao;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+
+	@Autowired
+	UtenteDao UtenteDao;
+	@Autowired
+	DomandaDao DomandaDao;
+	@Autowired
+	FeedbackDao FeedbackDao;
+	@Autowired
+	RuoloDao RuoloDao;
+	@Autowired
+	ModuloDao ModuloDao;
 
 }
