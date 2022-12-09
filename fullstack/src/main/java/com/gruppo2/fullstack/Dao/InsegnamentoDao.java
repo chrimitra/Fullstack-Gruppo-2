1fb3b18586c9ff6b3edc869820140bb34db0fae6
Christian <christianmitra7@gmail.com>
2022-12-09 13:15:56 +0100

    Registrazione.html - Modulo input quando viene selezionato il Ruolo Docente
    AdminController - Venogno salvati i dati nel db sia in utente e sia in
    insegnamento

--- fullstack/src/main/java/com/gruppo2/fullstack/Dao/InsegnamentoDao.java
+++ fullstack/src/main/java/com/gruppo2/fullstack/Dao/InsegnamentoDao.java
@@ -3,9 +3,11 @@ package com.gruppo2.fullstack.Dao;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import com.gruppo2.fullstack.model.Modulo;
+import com.gruppo2.fullstack.model.Insegnamento;
 
 @Repository
-public interface InsegnamentoDao extends CrudRepository<Modulo, Integer>{
+public interface InsegnamentoDao extends CrudRepository<Insegnamento, Integer>{
 
+	Insegnamento findByIdinsegnamento(Integer idinsegnamento);
+	
 }
