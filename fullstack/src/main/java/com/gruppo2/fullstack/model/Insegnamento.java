495dab287722203550cb9166972f6736d3350c9e
xenos96 <amime16tablet@gmail.com>
2022-12-09 12:15:42 +0100

    id insegnamento

--- fullstack/src/main/java/com/gruppo2/fullstack/model/Insegnamento.java
+++ fullstack/src/main/java/com/gruppo2/fullstack/model/Insegnamento.java
@@ -1,6 +1,9 @@
 package com.gruppo2.fullstack.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -8,6 +11,11 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "insegnamento")
 public class Insegnamento {
+	
+	
+	@Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+	public Integer idinsegnamento;
 	
 	@ManyToOne
 	@JoinColumn(name="idmodulo")
