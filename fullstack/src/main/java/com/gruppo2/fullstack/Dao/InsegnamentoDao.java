505f7bafe2eb4f73430adc1e2df9ef666621058d
Zir0Code <edoardoalessandro@gmail.com>
2022-12-09 16:20:27 +0100

    sistemato insegnamento

--- fullstack/src/main/java/com/gruppo2/fullstack/Dao/InsegnamentoDao.java
+++ fullstack/src/main/java/com/gruppo2/fullstack/Dao/InsegnamentoDao.java
@@ -3,27 +3,23 @@ package com.gruppo2.fullstack.Dao;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
 import com.gruppo2.fullstack.model.Insegnamento;
-<<<<<<< HEAD
 import com.gruppo2.fullstack.model.Modulo;
-=======
->>>>>>> branch 'main' of https://github.com/chrimitra/Fullstack-Gruppo-2.git
 
 @Repository
 public interface InsegnamentoDao extends CrudRepository<Insegnamento, Integer>{
 
-<<<<<<< HEAD
+
 	
 	
 	
 	@Query(value = "select modulo  from insegnamento \r\n"
 			+ "inner join modulo on insegnamento.idmodulo = modulo.idmodulo\r\n"
 			+ "where idutente= :x",nativeQuery=true)
-	public String Materia(Integer x) ;
-}
-=======
+	public String Materia(Integer x);
+	
+
+
 	Insegnamento findByIdinsegnamento(Integer idinsegnamento);
 	
 }
->>>>>>> branch 'main' of https://github.com/chrimitra/Fullstack-Gruppo-2.git
