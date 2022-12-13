9db83ec99e5ead58221fed3fce7dc1b2fa3922fd
Zir0Code <edoardoalessandro@gmail.com>
2022-12-13 09:44:57 +0100

    [Docente & Admin] sistemata la visualizzazione delle statistiche per domanda

--- fullstack/src/main/java/com/gruppo2/fullstack/controller/AdminController.java
+++ fullstack/src/main/java/com/gruppo2/fullstack/controller/AdminController.java
@@ -1,5 +1,6 @@
 package com.gruppo2.fullstack.controller;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
@@ -29,6 +30,7 @@ import com.gruppo2.fullstack.model.Insegnamento;
 import com.gruppo2.fullstack.model.Modulo;
 import com.gruppo2.fullstack.model.ReCaptchaResponse;
 import com.gruppo2.fullstack.model.Ruolo;
+import com.gruppo2.fullstack.model.Statistiche;
 import com.gruppo2.fullstack.model.Utente;
 
 
@@ -140,13 +142,17 @@ public class AdminController {
 	@RequestMapping("/reportDetails/{id}")
 	public ModelAndView reportDetails(HttpSession session, @PathVariable("id") Integer id) {
 		Utente loggedUser = (Utente) session.getAttribute("loggedUser");
-	
+		String Materia = ModuloDao.findByIdmodulo(id).modulo;
 		if (loggedUser != null) {
 			ModelAndView mavReportDetails = new ModelAndView();
 			mavReportDetails.setViewName("/reportDetails");
-			mavReportDetails.addObject("modulo", ModuloDao.findByIdmodulo(id));
+			mavReportDetails.addObject("materia", Materia);
 			mavReportDetails.addObject("feedback", FeedbackDao.dettagli(id));
 			mavReportDetails.addObject("domanda", DomandaDao.findAll());
+			
+			List<Statistiche> stats = Statistiche(Materia);
+			mavReportDetails.addObject("stats", stats);
+			
 			return mavReportDetails;
 			
 		} else {
@@ -326,33 +332,73 @@ public class AdminController {
 		
 		return "redirect:/admin/listaUtenti";
 	}
-	
-	
-	
-
-	
-	
-
-	
+//---------------------------------------------------------
+	public List<Statistiche> Statistiche(String Materia){
+		Double tot1=0.0;
+		Double tot2=0.0;
+		Double tot3=0.0;
+		Double tot4=0.0;
+		Double tot5=0.0;
+		Double media1=0.0;
+		Double media2=0.0;
+		Double media3=0.0;
+		Double media4=0.0;
+		Double media5=0.0;  //domandeMD
 		
+		List<Double> domande1 = FeedbackDao.Voti(Materia,1);
+		List<Double> domande2 = FeedbackDao.Voti(Materia,2);
+		List<Double> domande3 = FeedbackDao.Voti(Materia,3);
+		List<Double> domande4 = FeedbackDao.Voti(Materia,4);
+		List<Double> domande5 = FeedbackDao.Voti(Materia,5);
 		
+		List <Double> Totali = new ArrayList<>();
+		List <Double> Medie= new ArrayList<>();
 		
+		for(int i = 0; i < domande1.size(); i++) {
+	//------------------------------------------------------------1
+			tot1 += domande1.get(i);
+		}
+		media1 =tot1/domande1.size();
+		Totali.add(tot1);
+		Medie.add(media1);
+	//------------------------------------------------------------2		
+		for(int i = 0; i < domande2.size(); i++) {
+			tot2 += domande2.get(i);
+		}
+		media2 =tot2/domande2.size();
+		Totali.add(tot2);
+		Medie.add(media2);
+	//------------------------------------------------------------3		
+		for(int i = 0; i < domande3.size(); i++) {
+			tot3 += domande3.get(i);
+		}
+		media3 =tot3/domande3.size();
+		Totali.add(tot3);
+		Medie.add(media3);
+	//------------------------------------------------------------4		
+		for(int i = 0; i < domande4.size(); i++) {
+			tot4 += domande4.get(i);
+		}
+		media4 =tot4/domande4.size();
+		Totali.add(tot4);
+		Medie.add(media4);
+	//------------------------------------------------------------5		
+		for(int i = 0; i < domande5.size(); i++) {
+			tot5 += domande5.get(i);
+		}
+		media5 = tot5/domande5.size();
+		Totali.add(tot5);
+		Medie.add(media5);
+	//------------------------------------------------------------6
+		List<Domanda> ListaDomande =DomandaDao.listadomande();
+	//creazione di una lista di oggette (tipologia Statistiche)
+		List<Statistiche> stats = new ArrayList<>();
+		for(int i = 0; i < ListaDomande.size(); i++) {
+	//creazione e aggiunta dell'oggetto statistica alla lista stats
+			Statistiche x= new Statistiche(ListaDomande.get(i).domanda,Totali.get(i),Medie.get(i));
+			stats.add(x);
+		}
+		return stats;
+		}
+	
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- fullstack/src/main/java/com/gruppo2/fullstack/controller/InsegnanteController.java
+++ fullstack/src/main/java/com/gruppo2/fullstack/controller/InsegnanteController.java
@@ -53,70 +53,8 @@ public class InsegnanteController {
 			mavRisultati.setViewName("risultati");
 			mavRisultati.addObject("utente", loggedUser);
 			mavRisultati.addObject("materia", Materia);
-			Double tot1=0.0;
-			Double tot2=0.0;
-			Double tot3=0.0;
-			Double tot4=0.0;
-			Double tot5=0.0;
-			Double media1=0.0;
-			Double media2=0.0;
-			Double media3=0.0;
-			Double media4=0.0;
-			Double media5=0.0;  //domandeMD
-			
-			List<Double> domande1 = FeedbackDao.Voti(Materia,1);
-			List<Double> domande2 = FeedbackDao.Voti(Materia,2);
-			List<Double> domande3 = FeedbackDao.Voti(Materia,3);
-			List<Double> domande4 = FeedbackDao.Voti(Materia,4);
-			List<Double> domande5 = FeedbackDao.Voti(Materia,5);
-			
-			List <Double> Totali = new ArrayList<>();
-			List <Double> Medie= new ArrayList<>();
-			
-			for(int i = 0; i < domande1.size(); i++) {
-//------------------------------------------------------------1
-				tot1 += domande1.get(i);
-			}
-			media1 =tot1/domande1.size();
-			Totali.add(tot1);
-			Medie.add(media1);
-//------------------------------------------------------------2		
-			for(int i = 0; i < domande2.size(); i++) {
-				tot2 += domande2.get(i);
-			}
-			media2 =tot2/domande2.size();
-			Totali.add(tot2);
-			Medie.add(media2);
-//------------------------------------------------------------3		
-			for(int i = 0; i < domande3.size(); i++) {
-				tot3 += domande3.get(i);
-			}
-			media3 =tot3/domande3.size();
-			Totali.add(tot3);
-			Medie.add(media3);
-//------------------------------------------------------------4		
-			for(int i = 0; i < domande4.size(); i++) {
-				tot4 += domande4.get(i);
-			}
-			media4 =tot4/domande4.size();
-			Totali.add(tot4);
-			Medie.add(media4);
-//------------------------------------------------------------5		
-			for(int i = 0; i < domande5.size(); i++) {
-				tot5 += domande5.get(i);
-			}
-			media5 = tot5/domande5.size();
-			Totali.add(tot5);
-			Medie.add(media5);
-//------------------------------------------------------------6
-			List<Domanda> ListaDomande =DomandaDao.listadomande();
-//creazione di una lista di oggette (tipologia Statistiche)
-			List<Statistiche> stats = new ArrayList<>();
-			for(int i = 0; i < ListaDomande.size(); i++) {
-//creazione e aggiunta dell'oggetto statistica alla lista stats
-				Statistiche x= new Statistiche(ListaDomande.get(i).domanda,Totali.get(i),Medie.get(i));
-				stats.add(x);
-			}
+//----------------------------------------------------------------
+			List<Statistiche> stats = Statistiche(Materia);
 			mavRisultati.addObject("stats", stats);
 			return mavRisultati;
 		}
@@ -177,6 +115,82 @@ public class InsegnanteController {
 			return "redirect:/error404"; // Messaggio di errore che le password non sono uguali
 		}
 	}
+	
+	
+//--FUNZIONE STATISTICHE
+	
+	
+	public List<Statistiche> Statistiche(String Materia){
+	Double tot1=0.0;
+	Double tot2=0.0;
+	Double tot3=0.0;
+	Double tot4=0.0;
+	Double tot5=0.0;
+	Double media1=0.0;
+	Double media2=0.0;
+	Double media3=0.0;
+	Double media4=0.0;
+	Double media5=0.0;  //domandeMD
+	
+	List<Double> domande1 = FeedbackDao.Voti(Materia,1);
+	List<Double> domande2 = FeedbackDao.Voti(Materia,2);
+	List<Double> domande3 = FeedbackDao.Voti(Materia,3);
+	List<Double> domande4 = FeedbackDao.Voti(Materia,4);
+	List<Double> domande5 = FeedbackDao.Voti(Materia,5);
+	
+	List <Double> Totali = new ArrayList<>();
+	List <Double> Medie= new ArrayList<>();
+	
+	for(int i = 0; i < domande1.size(); i++) {
+//------------------------------------------------------------1
+		tot1 += domande1.get(i);
+	}
+	media1 =tot1/domande1.size();
+	Totali.add(tot1);
+	Medie.add(media1);
+//------------------------------------------------------------2		
+	for(int i = 0; i < domande2.size(); i++) {
+		tot2 += domande2.get(i);
+	}
+	media2 =tot2/domande2.size();
+	Totali.add(tot2);
+	Medie.add(media2);
+//------------------------------------------------------------3		
+	for(int i = 0; i < domande3.size(); i++) {
+		tot3 += domande3.get(i);
+	}
+	media3 =tot3/domande3.size();
+	Totali.add(tot3);
+	Medie.add(media3);
+//------------------------------------------------------------4		
+	for(int i = 0; i < domande4.size(); i++) {
+		tot4 += domande4.get(i);
+	}
+	media4 =tot4/domande4.size();
+	Totali.add(tot4);
+	Medie.add(media4);
+//------------------------------------------------------------5		
+	for(int i = 0; i < domande5.size(); i++) {
+		tot5 += domande5.get(i);
+	}
+	media5 = tot5/domande5.size();
+	Totali.add(tot5);
+	Medie.add(media5);
+//------------------------------------------------------------6
+	List<Domanda> ListaDomande =DomandaDao.listadomande();
+//creazione di una lista di oggette (tipologia Statistiche)
+	List<Statistiche> stats = new ArrayList<>();
+	for(int i = 0; i < ListaDomande.size(); i++) {
+//creazione e aggiunta dell'oggetto statistica alla lista stats
+		Statistiche x= new Statistiche(ListaDomande.get(i).domanda,Totali.get(i),Medie.get(i));
+		stats.add(x);
+	}
+	return stats;
+	}
+	
+	
+	
+	
 	
 	
 }
